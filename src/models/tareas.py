13f82d4511bb5b28d7e14bdc60330b60f0de7b0d
Karla-Michelle-Andres-Cruz <23308060610110@cetis61.edu.mx>
2026-03-23 10:47:34 -0600

    23/03/26

---
+++ src/models/tareas.py
@@ -0,0 +1,22 @@
+from .datebase import Database
+
+class TareaModel:
+    def __init__(self):
+        self.db = Database()
+        
+    def listar_por_usuario(self, usuario_id):
+        conn = self.db.get_connection()
+        cursor = conn.cursor(dictionary=True)
+        query = "SELECT * FROM tareas WHERE id_usuarios = %s ORDER BY fecha_limite ASC"
+        cursor.execute(query, (usuario_id,))
+        resultado = cursor.fetchall()
+        conn.close()
+        return resultado
+    
+    def crear(self, id_usuarios, titulo, descripcion, prioridad, clasificacion):
+        conn = self.db.get_connection()
+        cursor = conn.cursor()
+        query = """INSERT INTO tareas (id_usuarios, titulo, descripcion, prioridad, clasificacion) VALUES (%s, %s, %s, %s, %s)"""
+        cursor.execute(query, (id_usuarios, titulo, descripcion, prioridad, clasificacion))
+        conn.commit()
+        conn.close()
